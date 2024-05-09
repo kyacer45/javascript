17c753856ba7fa2d96e05c7c61d79fa6b7b1061e
kyacer45 <166542166+kyacer45@users.noreply.github.com>
2024-05-09 11:16:12 +0545

    i added aryal

---
+++ main.js
@@ -0,0 +1,87 @@
+// console.log("This is console print");
+// let num1 = 28;
+// let num2 = 19;
+// console.log(num1 + num2);
+
+// let _myName = "sakar";
+// console.log(_myName);
+// let $function = "Concern";
+
+// let college = "Butwal Kalika Campus";
+// console.log(college);
+
+// let course = "BCA";
+// console.log(course);
+// let student = "Kaushal";
+// console.log(student);
+
+// console.log(6+3);
+// console.log("This is console print after alert");
+// let age = prompt("Enter your age");
+// if(age >= 18){
+//   console.log("you can vote");
+// }
+// else{
+//   console.log("you cannot vote");
+// }
+
+// alert("hellow world"); 
+
+// let num3 = 3;
+// let myName = "dinesh";
+// console.log(typeof num3); // Output: "number"
+// console.log(typeof myName); // Output: "string"
+
+// let age = 18;
+
+// let isEnough = age >= 18;
+// console.log(isEnough, "this is enough"); // Output: true "this is enough"
+
+// let num8 = 10;
+// let num9 = num8;
+// console.log(num8, "this is num8"); // Output: 10 "this is num8"
+
+// console.log("Corrected console.log statement"); // Added a message to the empty console.log()
+
+// const myCollege = "Kalika"
+// const bithYear = 2000
+// let myClass = "BCA 3"
+
+// const PI= 3.14;
+// const API_URL = "http://localhost:3000"
+// //const students_url = API_URL + "/students" 
+// API_URL = "jkdkdkd"
+// console.log(API_URL)
+// let heyKalika = "kalika"
+ 
+// console.log("kalika")
+// console.log("Nepal")
+
+// let name1= "Prashant"
+// let name2=name1
+
+// console.log(name2,"first")
+// name1="sagar"
+
+// console.log(name2,"second")
+
+// const collegeName="kalika"
+// console.log(collegeName)
+// const batch= 2020 
+// console.log(batch)
+// let studentName=" Kaushal"
+// console.log(studentName)
+// const uniRegNo= 3322114
+// console.log(uniRegNo)
+
+let name=" Kaushal."
+let study=" BCA"
+let location=" Butwal"
+console.log('My name')
+let myName="ankit"
+console.log(myName)
+console.log("sakr")
+console.log("aryal")
+
+
+
